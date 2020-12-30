b9b668f8ce023c87b55c7f71df4d3e4793e4c95b
Mdshaffat <shaffatbd@gmail.com>
2020-12-30 14:18:34 +0600

    searchFunctionalityAdded

--- client/src/app/shared/shared.module.ts
+++ client/src/app/shared/shared.module.ts
@@ -1,19 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {PaginationModule} from 'ngx-bootstrap/pagination';
-import { PagingHeaderComponent } from './Components/paging-header/paging-header.component'; 
+import { PagingHeaderComponent } from './Components/paging-header/paging-header.component';
+import { PagerComponent } from './Components/pager/pager.component'; 
 
 
 
 @NgModule({
-  declarations: [PagingHeaderComponent],
+  declarations: [PagingHeaderComponent, PagerComponent],
   imports: [
     CommonModule,
     PaginationModule.forRoot()
   ],
   exports: [
     PaginationModule,
-    PagingHeaderComponent
+    PagingHeaderComponent,
+    PagerComponent
   ]
 })
 export class SharedModule { }
